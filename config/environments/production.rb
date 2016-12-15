600c562c9772caf58bcba5207c9bcdd899da87e0
Jose Luis Saavedra Castillo <jsaavedrac1@gmail.com>
2016-12-15 17:28:18 -0500

    envio de emails de lo post

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   #Mailer
-  config.action_mailer.default_url_options = { host: 'whconsultores' }
+  config.action_mailer.default_url_options = { host: 'whconsultores.com' }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
